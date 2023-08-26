342f7057a16d4b39148e39b6daf4e9f20d9db8e9
kuviman <kuviman@gmail.com>
2023-08-26 20:09:53 +0400

    separate effect args

--- src/effects.rs
+++ src/effects.rs
@@ -18,49 +18,17 @@ trait FileSystem {
     async fn read_file(&self, path: impl AsRef<Path>) -> Result<String, std::io::Error>;
 }
 
-async fn simple(ctx: &(impl Log + Cancel<i32>)) -> i32 {
-    ctx.log("hello").await;
-    ctx.log("world").await;
-    ctx.cancel(4).await;
-    ctx.log("this will never be logged").await;
+async fn simple(log: &impl Log, cancel: &impl Cancel<i32>) -> i32 {
+    log.log("hello").await;
+    log.log("world").await;
+    cancel.cancel(4).await;
+    log.log("this will never be logged").await;
     10
 }
 
-fn extend_context_with_cancel<'a, T: 'a>(
-    ctx: &'a (impl Log + FileSystem),
-    cancel: &'a impl Cancel<T>,
-) -> impl Log + FileSystem + Cancel<T> + 'a {
-    struct ContextWithCancel<'a, Inner, T> {
-        inner: &'a Inner,
-        cancel: &'a T,
-    }
-
-    #[async_trait(?Send)]
-    impl<T, Inner: Log> Log for ContextWithCancel<'_, Inner, T> {
-        async fn log(&self, message: &str) {
-            self.inner.log(message).await
-        }
-    }
-
-    #[async_trait(?Send)]
-    impl<Inner: FileSystem, T> FileSystem for ContextWithCancel<'_, Inner, T> {
-        async fn read_file(&self, path: impl AsRef<Path>) -> Result<String, std::io::Error> {
-            self.inner.read_file(path).await
-        }
-    }
-
-    #[async_trait(?Send)]
-    impl<'a, T: 'a, C: Cancel<T>, Inner> Cancel<T> for ContextWithCancel<'a, Inner, C> {
-        async fn cancel(&self, value: T) -> Infallible {
-            self.cancel.cancel(value).await
-        }
-    }
-    ContextWithCancel { inner: ctx, cancel }
-}
-
-async fn combined(ctx: &(impl Log + FileSystem)) {
-    let passwords = ctx.read_file("passwords.txt").await.unwrap();
-    ctx.log(&format!("passwords are: {passwords:?}")).await;
+async fn combined(log: &impl Log, fs: &impl FileSystem) {
+    let passwords = fs.read_file("passwords.txt").await.unwrap();
+    log.log(&format!("passwords are: {passwords:?}")).await;
 
     let x: _ = reset(|reset_handler| async move {
         struct CancelHandler<'a, T> {
@@ -82,28 +50,26 @@ async fn combined(ctx: &(impl Log + FileSystem)) {
         let cancel = CancelHandler {
             reset_handler: RefCell::new(Some(reset_handler)),
         };
-        let ctx = extend_context_with_cancel(ctx, &cancel);
-        let ctx = &ctx;
-
-        simple(ctx).await
+        simple(log, &cancel).await
     })
     .await;
     dbg!(x);
 }
 
 pub async fn main() {
-    struct Context;
+    struct LogHandler;
     #[async_trait(?Send)]
-    impl Log for Context {
+    impl Log for LogHandler {
         async fn log(&self, message: &str) {
             println!("{message}");
         }
     }
+    struct FileSystemHandler;
     #[async_trait(?Send)]
-    impl FileSystem for Context {
+    impl FileSystem for FileSystemHandler {
         async fn read_file(&self, _path: impl AsRef<Path>) -> Result<String, std::io::Error> {
             Ok("hello, there!".to_owned())
         }
     }
-    combined(&Context).await;
+    combined(&LogHandler, &FileSystemHandler).await;
 }
