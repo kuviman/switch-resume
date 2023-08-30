a5e0cd3262840c2b9be24ba9b2765827e2adfc8a
kuviman <kuviman@gmail.com>
2023-08-31 01:05:05 +0400

    internal renamings

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@ type Continuation<'a, T> = Pin<Box<dyn Future<Output = T> + 'a>>;
 type BoxPauseHandler<'a, T> = Box<dyn FnOnce(Continuation<'a, T>) -> Continuation<'a, T> + 'a>;
 
 pub struct Task<'a, T: 'a> {
-    pause_handler_sender: async_channel::Sender<BoxPauseHandler<'a, T>>,
+    switch_sender: async_channel::Sender<BoxPauseHandler<'a, T>>,
 }
 
 impl<'a, T> Task<'a, T> {
@@ -51,7 +51,7 @@ impl<'a, T> Task<'a, T> {
         f: F,
     ) -> ResumeArg {
         let (mut resume_arg_sender, resume_arg_receiver) = async_oneshot::oneshot();
-        self.pause_handler_sender
+        self.switch_sender
             .try_send(Box::new(move |continuation: Continuation<'a, T>| {
                 Box::pin(f(Box::new(move |arg| {
                     resume_arg_sender.send(arg).expect("WTF");
@@ -70,11 +70,9 @@ impl<'a, T> Task<'a, T> {
 pub async fn run<'a, T: 'a, Fut: Future<Output = T> + 'a>(
     f: impl FnOnce(Task<'a, T>) -> Fut + 'a,
 ) -> T {
-    let (pause_handler_sender, pause_handler_receiver) = async_channel::bounded(1);
-    let handler = Task {
-        pause_handler_sender,
-    };
-    let mut continuation: Continuation<'a, T> = Box::pin(f(handler));
+    let (switch_sender, switch_receiver) = async_channel::bounded(1);
+    let task = Task { switch_sender };
+    let mut continuation: Continuation<'a, T> = Box::pin(f(task));
     loop {
         let poll = Future::poll(
             continuation.as_mut(),
@@ -83,7 +81,7 @@ pub async fn run<'a, T: 'a, Fut: Future<Output = T> + 'a>(
         match poll {
             Poll::Ready(result) => return result,
             Poll::Pending => {
-                match pause_handler_receiver.try_recv() {
+                match switch_receiver.try_recv() {
                     Ok(pause_handler) => {
                         continuation = pause_handler(continuation);
                     }
